6f1d577fba68ad14b1ecb07e162d4605dd8eecf7
powerivq <poweriv.q@gmail.com>
2020-08-26 11:31:13 -0700

    exp

--- ads/google/a4a/utils.js
+++ ads/google/a4a/utils.js
@@ -206,6 +206,7 @@ export function googleBlockParameters(a4a, roundLocations, opt_experimentIds) {
   if (opt_experimentIds) {
     eids = mergeExperimentIds(opt_experimentIds, eids);
   }
+  eids = mergeExperimentIds([21067243], eids);
   return {
     'adf': DomFingerprint.generate(adElement),
     'nhd': iframeDepth,
